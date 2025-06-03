e59659ccce1b60bda7d69a1519a0f57a123bbaf9
915-Pavel-Dragos <dragoscristian.pavel@gmail.com>
2025-06-03 23:06:04 +0300

    Backend Auth Done

---
+++ backend/app_backend/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView
+
+
+urlpatterns = [
+    path('register/', UserRegistrationAPIView.as_view(), name='register-view'),
+    path('login/', UserLoginAPIView.as_view(), name='login-view'),
+    path('logout/', UserLogoutAPIView.as_view(), name='logout-view'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token-refresh')
+]
+
